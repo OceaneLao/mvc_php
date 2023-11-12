a16cd70714c3f9710a3af6cb15001044191e7c3f
Lao Oceane <oceane.lao1@gmail.com>
2023-11-12 23:02:23 +0100

    add php mailer files

---
+++ controllers/controller_mail.php
@@ -0,0 +1,50 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require './services/PHPMailer-master/src/Exception.php';
+require './services/PHPMailer-master/src/PHPMailer.php';
+require './services/PHPMailer-master/src/SMTP.php';
+
+$mail = new PHPMailer(true);
+
+try {
+$mail->isSMTP();
+$mail->Host = 'sandbox.smtp.mailtrap.io';
+$mail->SMTPAuth = true;
+$mail->Port = 2525;
+$mail->Username = 'dccaf11b3b7d34';
+$mail->Password = 'ad1548e6db64f1';
+
+ //Recipients
+ $mail->setFrom('from@example.com', 'Mailer');
+ $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
+ $mail->addAddress('ellen@example.com');               //Name is optional
+ $mail->addReplyTo('info@example.com', 'Information');
+ $mail->addCC('cc@example.com');
+ $mail->addBCC('bcc@example.com');
+
+ //Attachments
+ $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+ $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+ //Content
+ $mail->isHTML(true);                                  //Set email format to HTML
+ $mail->Subject = 'Here is the subject';
+ $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+ $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+ $mail->send();
+ echo 'Message has been sent';
+} catch (Exception $e) {
+ echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+// $mail = mail('oceane.lao1@gmail.com','Hello','Je fais un test','From: moi@exemple.com');
+
+// if ($mail){
+//     echo "Merci";
+// }else{
+//     echo "Erreur";
+// }
+
+include "./views/layout.phtml";
