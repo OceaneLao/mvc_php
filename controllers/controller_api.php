f2196a8c9d9d7fd17e132559cec5733cebe003c4
OceaneLao <oceane.lao1@gmail.com>
2023-11-13 15:30:25 +0100

    ctrl adminlist

--- controllers/controller_api.php
+++ controllers/controller_api.php
@@ -1,5 +1,6 @@
 <?php
 require_once ("./models/Picture.php");
-$pictures = Picture::getAll();
+$db = new Database();
+$pictures = $db->query("SELECT * FROM pictures ORDER BY id DESC LIMIT 10");
 header('content-type:application/json');
 echo json_encode($pictures);
